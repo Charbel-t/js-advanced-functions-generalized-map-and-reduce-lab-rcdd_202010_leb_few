f1924bdaa0d4b1e762305b7f8b891197d651bd72
Charbel t <charbel2000.52@gmail.com>
2020-12-07 10:04:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function map(src, callback){
   return r;
 }
 
-function reduce(src, callback, starting){
+function reduce(src, cb, starting){
   let r= (!!starting) ? starting : src[0];
   let i= (!!starting) ? 0 : 1;
   for(;i<src.length;i++){
